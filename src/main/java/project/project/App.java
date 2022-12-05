6ef02e26f77d7c44873201b487e67f00ef1e2552
Admin <Admin@DESKTOP-QG2DJLM>
2022-12-05 19:26:12 +0530

    next commit

--- src/main/java/project/project/App.java
+++ src/main/java/project/project/App.java
@@ -6,27 +6,18 @@ package project.project;
  */
 public class App 
 {
-	public class SleepExp1 extends Thread  
-	{    
-	    public void run()  
-	    {    
-	    	int num=8;
-	        for(int i=1;i<5;i++)  
-	        {    
-	            try  
-	            {  
-	                Thread.sleep(500);  
-	            }catch(InterruptedException e){System.out.println(e);}    
-	            for(int j=1;j<=10;j++) {
-	            	System.out.printf("%d * %d = %d\n",num,j,num*j);
-	            }
-	        }    
-	    }    
-	    public void main(String args[])
-	    {    
-	        SleepExp1 t1=new SleepExp1();    
-    
-	        t1.start();   
-	    }    
-	}  
+	static class TestSleepMethod1 extends Thread{ 
+		int num=8;
+		 public void run(){    
+		  for(int i=1;i<5;i++){   
+		  // the thread will sleep for the 500 milli seconds   
+		    try{Thread.sleep(500);}catch(InterruptedException e){System.out.println(e);}    
+		    System.out.printf("%d * %d = %d\n",num,i,num*i);    
+		  }    
+		 }    
+		 public static void main (String args[]){    
+		  TestSleepMethod1 t1=new TestSleepMethod1();    
+		  t1.start();      
+		 }    
+	}
 }
