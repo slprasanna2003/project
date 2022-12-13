3e1130e13169a7e380e3bae5b35e9584af42053f
Admin <Admin@DESKTOP-QG2DJLM>
2022-12-13 06:30:11 +0530

    next commit

--- src/main/java/project/project/App.java
+++ src/main/java/project/project/App.java
@@ -8,16 +8,10 @@ public class App extends Thread
 { 
 		 public int multiply(int num) throws InterruptedException{ 
 			 int i;
-			 int p;
 		  for(i=1;i<=10;i++){   
-		    Thread.sleep(1000); 
-		    p=num*i;
-		    System.out.printf("%d * %d = %d\n",num,i,p);
+		    Thread.sleep(1000);
+		    System.out.printf("%d * %d = %d\n",num,i,num*i);
 		  }
 		  return 0;
 		 }      
-		 public static void main(String args[]) throws InterruptedException {
-			 App m=new App();
-			 m.multiply(8);
-		 }
 }
