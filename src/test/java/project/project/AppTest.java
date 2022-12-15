cffdfbba74374034b9c57a0ab333d743c0e20b85
Admin <Admin@DESKTOP-QG2DJLM>
2022-12-15 20:41:52 +0530

    next commit

--- src/test/java/project/project/AppTest.java
+++ src/test/java/project/project/AppTest.java
@@ -18,14 +18,6 @@ public class AppTest
     {
         App a=new App();
         assertEquals(8, a.multiply(8));
-        assertEquals(16, a.multiply(8));
-        assertEquals(24, a.multiply(8));
-        assertEquals(32, a.multiply(8));
-        assertEquals(40, a.multiply(8));
-        assertEquals(48, a.multiply(8));
-        assertEquals(56, a.multiply(8));
-        assertEquals(64, a.multiply(8));
-        assertEquals(72, a.multiply(8));
-        assertEquals(80, a.multiply(8));
+        
      }
 }
